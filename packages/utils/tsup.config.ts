678ddb986907a872cfd015e30bfcfaf6d8e44fa5
Dian Setiawan <diansetiawan2121@gmail.com>
2026-03-30 03:15:52 +0700

    feat: update package.json and tsup.config.ts for CommonJS support and type definitions

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/fp/index.ts', 'src/common/index.ts'],
-  format: ['esm'],
+  format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
